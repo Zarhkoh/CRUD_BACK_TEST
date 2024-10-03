66bdb59f1a0297d79f06d2d1141167f644217396
AMichelas <120266869+AMichelas@users.noreply.github.com>
2024-10-03 14:25:58 +0200

    work on change mail and password

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,4 +1,3 @@
-// app/controllers/auth.controller.js
 const db = require("../models");
 const config = require("../config/auth.config");
 const User = db.user;
@@ -10,6 +9,7 @@ var jwt = require("jsonwebtoken");
 var bcrypt = require("bcryptjs");
 
 exports.signup = (req, res) => {
+  // Save User to Database
   User.create({
     username: req.body.username,
     email: req.body.email,
@@ -29,6 +29,7 @@ exports.signup = (req, res) => {
           });
         });
       } else {
+        // user role = 1
         user.setRoles([1]).then(() => {
           res.send({ message: "User registered successfully!" });
         });
@@ -50,7 +51,11 @@ exports.signin = (req, res) => {
         return res.status(404).send({ message: "User Not found." });
       }
 
-      var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+      var passwordIsValid = bcrypt.compareSync(
+        req.body.password,
+        user.password
+      );
+
       if (!passwordIsValid) {
         return res.status(401).send({
           accessToken: null,
@@ -58,11 +63,13 @@ exports.signin = (req, res) => {
         });
       }
 
-      const token = jwt.sign({ id: user.id }, config.secret, {
-        algorithm: 'HS256',
-        allowInsecureKeySizes: true,
-        expiresIn: 86400, // 24 hours
-      });
+      const token = jwt.sign({ id: user.id },
+                              config.secret,
+                              {
+                                algorithm: 'HS256',
+                                allowInsecureKeySizes: true,
+                                expiresIn: 86400, // 24 hours
+                              });
 
       var authorities = [];
       user.getRoles().then(roles => {
@@ -81,9 +88,4 @@ exports.signin = (req, res) => {
     .catch(err => {
       res.status(500).send({ message: err.message });
     });
-};
-
-exports.signout = (req, res) => {
-  // Logique pour la déconnexion
-  res.status(200).send({ message: "Déconnexion réussie." });
 };
