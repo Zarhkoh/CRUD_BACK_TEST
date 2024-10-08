8bf5fbef2a086be75beac0f02b15980d14df41d9
AMichelas <120266869+AMichelas@users.noreply.github.com>
2024-10-08 16:03:14 +0200

    Mailer OK

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -2,8 +2,8 @@ const nodemailer = require("nodemailer");
 const db = require("../models");
 const config = require("../config/auth.config");
 const User = db.user;
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
+const Role = db.role;
+
 
 // Configuration de l'envoi d'e-mail via Gmail
 const transporter = nodemailer.createTransport({
@@ -13,6 +13,93 @@ const transporter = nodemailer.createTransport({
     pass: process.env.PASSWORD,
   },
 });
+
+const Op = db.Sequelize.Op;
+
+var jwt = require("jsonwebtoken");
+var bcrypt = require("bcryptjs");
+
+const signup = (req, res) => {
+  // Save User to Database
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, 8)
+  })
+    .then(user => {
+      if (req.body.roles) {
+        Role.findAll({
+          where: {
+            name: {
+              [Op.or]: req.body.roles
+            }
+          }
+        }).then(roles => {
+          user.setRoles(roles).then(() => {
+            res.send({ message: "User registered successfully!" });
+          });
+        });
+      } else {
+        // user role = 1
+        user.setRoles([1]).then(() => {
+          res.send({ message: "User registered successfully!" });
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({ message: err.message });
+    });
+};
+
+const signin = (req, res) => {
+  User.findOne({
+    where: {
+      username: req.body.username
+    }
+  })
+    .then(user => {
+      if (!user) {
+        return res.status(404).send({ message: "User Not found." });
+      }
+
+      var passwordIsValid = bcrypt.compareSync(
+        req.body.password,
+        user.password
+      );
+
+      if (!passwordIsValid) {
+        return res.status(401).send({
+          accessToken: null,
+          message: "Invalid Password!"
+        });
+      }
+
+      const token = jwt.sign({ id: user.id },
+                              config.secret,
+                              {
+                                algorithm: 'HS256',
+                                allowInsecureKeySizes: true,
+                                expiresIn: 86400, // 24 hours
+                              });
+
+      var authorities = [];
+      user.getRoles().then(roles => {
+        for (let i = 0; i < roles.length; i++) {
+          authorities.push("ROLE_" + roles[i].name.toUpperCase());
+        }
+        res.status(200).send({
+          id: user.id,
+          username: user.username,
+          email: user.email,
+          roles: authorities,
+          accessToken: token
+        });
+      });
+    })
+    .catch(err => {
+      res.status(500).send({ message: err.message });
+    });
+};
 
 // Fonction pour demander la réinitialisation du mot de passe
 const requestPasswordReset = async (req, res) => {
@@ -107,45 +194,6 @@ const changePassword = async (req, res) => {
     user.password = bcrypt.hashSync(newPassword, 8);
     await user.save();
     return res.status(200).send({ message: "Mot de passe changé avec succès!" });
-  } catch (error) {
-    return res.status(500).send({ message: error.message });
-  }
-};
-
-// Fonction d'inscription
-const signup = async (req, res) => {
-  try {
-    const user = await User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, 8),
-    });
-    return res.send({ message: "Utilisateur enregistré avec succès!" });
-  } catch (error) {
-    return res.status(500).send({ message: error.message });
-  }
-};
-
-// Fonction de connexion
-const signin = async (req, res) => {
-  try {
-    const user = await User.findOne({ where: { username: req.body.username } });
-    if (!user) {
-      return res.status(404).send({ message: "Utilisateur non trouvé." });
-    }
-
-    const passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
-    if (!passwordIsValid) {
-      return res.status(401).send({ accessToken: null, message: "Mot de passe invalide!" });
-    }
-
-    const token = jwt.sign({ id: user.id }, config.secret, { expiresIn: 86400 }); // 24 heures
-    return res.status(200).send({
-      id: user.id,
-      username: user.username,
-      email: user.email,
-      accessToken: token,
-    });
   } catch (error) {
     return res.status(500).send({ message: error.message });
   }
