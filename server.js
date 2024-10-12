2fc897c7e135b01a2f67930698c4598b30aaef68
AMichelas <120266869+AMichelas@users.noreply.github.com>
2024-10-12 12:52:58 +0200

    modif origin

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 // Configurer les options de CORS
 var corsOptions = {
   credentials: true,
-  origin: "https://https://backshowmylife.vercel.app/"
+  origin: "https://https://showmylife.vercel.app"
 };
 
 app.use(cors(corsOptions));
